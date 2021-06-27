d417f2e745e62d969359e77d61d5438380097edb
Mishrilal Chhaparia <mishrilalchhaparia@gmail.com>
2021-06-27 15:22:59 +0530

    Created Splash Screen and Login Activity

--- app/src/main/java/io/github/mishrilal/foodrunner/LoginActivity.kt
+++ app/src/main/java/io/github/mishrilal/foodrunner/LoginActivity.kt
@@ -2,10 +2,11 @@ package io.github.mishrilal.foodrunner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/io/github/mishrilal/foodrunner/SplashActivity.kt
@@ -0,0 +1,20 @@
+package io.github.mishrilal.foodrunner
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+        }, 2000)
+    }
+}
