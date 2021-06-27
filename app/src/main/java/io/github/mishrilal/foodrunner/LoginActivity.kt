5ce0187574915d807e33d51e4ee4f86ab28b0745
Mishrilal Chhaparia <mishrilalchhaparia@gmail.com>
2021-06-27 17:39:27 +0530

    Login Activity Completed

--- app/src/main/java/io/github/mishrilal/foodrunner/LoginActivity.kt
+++ app/src/main/java/io/github/mishrilal/foodrunner/LoginActivity.kt
@@ -1,12 +1,78 @@
 package io.github.mishrilal.foodrunner
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var etMobileNumber: EditText
+    lateinit var etPassword: EditText
+    lateinit var txtForgotPassword: TextView
+    lateinit var txtRegister: TextView
+    lateinit var btnLogin: Button
+
+    private val validMobileNumber = "1234567890"
+    private val validPassword = "password"
+
+    lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sharedPreferences =
+            getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
+
+        val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
+
         setContentView(R.layout.activity_login)
+
+//        if (isLoggedIn) {
+//            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
+
+        title = "Food Runner"
+
+        etMobileNumber = findViewById(R.id.etMobileNumber)
+        etPassword = findViewById(R.id.etPassword)
+        txtForgotPassword = findViewById(R.id.txtForgotPassword)
+        txtRegister = findViewById(R.id.txtRegister)
+        btnLogin = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val mobileNumber = etMobileNumber.text.toString()
+            val password = etPassword.text.toString()
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            if (mobileNumber == validMobileNumber && password == validPassword) {
+                savePreferences()
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this@LoginActivity, "Incorrect Credentials", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+        txtForgotPassword.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, ForgotPasswordActivity::class.java))
+        }
+
+        txtRegister.setOnClickListener{
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+        }
+
+    }
+
+    fun savePreferences() {
+        sharedPreferences.edit().putBoolean("isLoggedIn", true).apply()
     }
 }
