980bbd6b4f299959de82996e6976ec932edb49a2
Mishrilal Chhaparia <mishrilalchhaparia@gmail.com>
2021-07-14 13:11:29 +0530

    Login through API

--- app/src/main/java/io/github/mishrilal/foodrunner/activity/LoginActivity.kt
+++ app/src/main/java/io/github/mishrilal/foodrunner/activity/LoginActivity.kt
@@ -1,15 +1,25 @@
 package io.github.mishrilal.foodrunner.activity
-
+// TODO: Add progress Bar for login btn
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import io.github.mishrilal.foodrunner.R
+import io.github.mishrilal.foodrunner.util.ConnectionManager
+import org.json.JSONException
+import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
 
@@ -21,6 +31,8 @@ class LoginActivity : AppCompatActivity() {
 
     private val validMobileNumber = "1234567890"
     private val validPassword = "password"
+
+    var mobilePattern = "[6-9][0-9]{9}"
 
     lateinit var sharedPreferences: SharedPreferences
 
@@ -52,15 +64,97 @@ class LoginActivity : AppCompatActivity() {
             val mobileNumber = etMobileNumber.text.toString()
             val password = etPassword.text.toString()
             val intent = Intent(this@LoginActivity, MainActivity::class.java)
-            if (mobileNumber == validMobileNumber && password == validPassword) {
-                savePreferences()
-                intent.putExtra("MobileNumber", mobileNumber)
-                intent.putExtra("Password", password)
-                startActivity(intent)
-                finish()
-            } else {
-                Toast.makeText(this@LoginActivity, "Incorrect Credentials", Toast.LENGTH_LONG)
-                    .show()
+
+            if (validations(etMobileNumber.text.toString(), etPassword.text.toString())) {
+
+                if (ConnectionManager().isNetworkAvailable(this@LoginActivity)) {
+
+                    val queue = Volley.newRequestQueue(this@LoginActivity)
+                    val url = "http://13.235.250.119/v2/login/fetch_result/"
+                    val jsonParams = JSONObject()
+                    jsonParams.put("mobile_number", mobileNumber)
+                    jsonParams.put("password", password)
+
+                    val jsonObjectRequest =
+                        object : JsonObjectRequest(
+                            Request.Method.POST,
+                            url,
+                            jsonParams,
+                            Response.Listener {
+                                try {
+                                    val data = it.getJSONObject("data")
+                                    val success = data.getBoolean("success")
+                                    if (success) {
+                                        btnLogin.isEnabled = false
+                                        btnLogin.isClickable = false
+
+                                        val response = data.getJSONObject("data")
+                                        sharedPreferences.edit()
+                                            .putString("user_id", response.getString("user_id"))
+                                            .apply()
+                                        sharedPreferences.edit()
+                                            .putString("user_name", response.getString("name"))
+                                            .apply()
+                                        sharedPreferences.edit().putString(
+                                            "user_mobile_number",
+                                            response.getString("mobile_number")
+                                        ).apply()
+                                        sharedPreferences.edit()
+                                            .putString(
+                                                "user_address",
+                                                response.getString("address")
+                                            )
+                                            .apply()
+                                        sharedPreferences.edit()
+                                            .putString("user_email", response.getString("email"))
+                                            .apply()
+
+                                        savePreferences()
+                                        startActivity(intent)
+                                        finish()
+                                    } else {
+                                        Toast.makeText(
+                                            this@LoginActivity,
+                                            "Invalid Credentials",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                } catch (e: JSONException) {
+                                    e.printStackTrace()
+                                }
+                            },
+                            Response.ErrorListener {
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    "Volley error occurred",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }) {
+                            override fun getHeaders(): MutableMap<String, String> {
+                                val headers = HashMap<String, String>()
+                                headers["Content-type"] = "application/json"
+                                headers["token"] = "9bf534118365f1"
+                                return headers
+                            }
+                        }
+                    queue.add(jsonObjectRequest)
+                } else {
+                    val dialog = AlertDialog.Builder(this@LoginActivity)
+                    dialog.setTitle("Error")
+                    dialog.setMessage("Internet Connection Found")
+                    dialog.setPositiveButton("Open Settings") { text, listener ->
+                        val settingsIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                        startActivity(settingsIntent)
+                        this.finish()
+
+                    }
+                    dialog.setNegativeButton("Exit") { text, listener ->
+
+                        ActivityCompat.finishAffinity(this@LoginActivity)
+                    }
+                    dialog.create()
+                    dialog.show()
+                }
             }
         }
 
@@ -68,7 +162,7 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this@LoginActivity, ForgotPasswordActivity::class.java))
         }
 
-        txtRegister.setOnClickListener{
+        txtRegister.setOnClickListener {
             startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
         }
 
@@ -80,5 +174,32 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         finishAffinity()
+    }
+
+    private fun validations(phone: String, password: String): Boolean {
+
+        if (phone.isEmpty() && password.isEmpty()) {
+            Toast.makeText(this@LoginActivity, "Enter Credentials", Toast.LENGTH_SHORT).show()
+            return false
+        } else if (phone.isEmpty()) {
+            Toast.makeText(this@LoginActivity, "Enter Mobile Number", Toast.LENGTH_SHORT).show()
+            return false
+        } else {
+            return if (password.isEmpty()) {
+                Toast.makeText(this@LoginActivity, "Enter Password", Toast.LENGTH_SHORT).show()
+                false
+            } else {
+                if (!phone.trim().matches(mobilePattern.toRegex())) {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Enter a valid Mobile number",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                    false
+                } else
+                    true
+            }
+        }
     }
 }
